438bd4638d27ca322cb1b48aa21fd3ffbdb2f036
MSI\なか <rikuto.0228@icloud.com>
2025-11-27 11:08:21 +0900

    ペットボトルゴミとカレンダーの移動の追加

--- gouyoku/Src/Object/Actor/Object/Calender.cpp
+++ gouyoku/Src/Object/Actor/Object/Calender.cpp
@@ -53,9 +53,9 @@ void Calender::InitLoad(void)
 
 void Calender::InitTransform(void)
 {
-	pos_ = { 0.000370f,200.0f,860.0f };
+	pos_ = { -2006.825439f,180.0f,-1090.455444f };
 	scale_ = { 0.7f,0.7f,0.7f };
-	angle_ = { 0.0f,0.0f * DX_PI_F / 180.0f,0.0f };
+	angle_ = { 0.0f,180.0f * DX_PI_F / 180.0f,0.0f };
 
 	for (int i = 0; i < static_cast<int>(Calender::CALENDER::MAX); i++)
 	{
