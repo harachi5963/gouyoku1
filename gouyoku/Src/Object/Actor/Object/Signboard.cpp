f6cd2a93c2b5b024cfc8a76e5004862f9bb5ff9c
AzureAD\eguchitakumi <ta15yu11.tt@gmail.com>
2025-11-28 12:04:38 +0900

    看板配置

--- gouyoku/Src/Object/Actor/Object/Signboard.cpp
+++ gouyoku/Src/Object/Actor/Object/Signboard.cpp
@@ -1,4 +1,6 @@
 #include "Signboard.h"
+#include "../../Common/AnimationController.h"
+#include "../../../Input/InputManager.h"
 
 Signboard::Signboard()
 {
@@ -11,14 +13,12 @@ Signboard::~Signboard()
 void Signboard::Update(void)
 {
 	ActorBase::Update();
-
 }
 
 void Signboard::Draw(void)
 {
 	ActorBase::Draw();
 }
-
 
 void Signboard::Release(void)
 {
@@ -28,14 +28,13 @@ void Signboard::Release(void)
 void Signboard::InitLoad(void)
 {
 	modelId_ = MV1LoadModel("Data/object/signboard/signboard.mv1");
-
 }
 
 void Signboard::InitTransform(void)
 {
-	pos_ = { 916.0f,0.0f,-10.0f };
-	scale_ = { 0.5f,0.5f,0.5f };
-	angle_ = { 0.0f,300.0f,0.0f };
+	pos_ = { -463.0f,155.0f,-380.0f };
+	scale_ = { 1.2f,1.2f,1.2f };
+	angle_ = { 0.0f,0.0f,0.0f };
 
 	// サイズ変更
 	MV1SetScale(modelId_, scale_);
